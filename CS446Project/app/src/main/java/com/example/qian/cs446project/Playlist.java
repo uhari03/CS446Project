b6614d2408aaa2b7dce6c20a75a659026c09ea05
Hari Udhayakumar <u.hari03@gmail.com>
2018-02-24 21:00:32 -0500

    Made Playlist and Song implement Parcelable

--- CS446Project/app/src/main/java/com/example/qian/cs446project/Playlist.java
+++ CS446Project/app/src/main/java/com/example/qian/cs446project/Playlist.java
@@ -1,5 +1,8 @@
 package com.example.qian.cs446project;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.LinkedList;
 
 /**
@@ -19,7 +22,8 @@ public class Playlist {
         this.playlistID = playlistID;
     }
 
-    public String toString() { return getPlaylistName(); }
+    @Override
+    public String toString() { return playlistName; }
 
     public String getPlaylistName() {
         return playlistName;
@@ -33,5 +37,39 @@ public class Playlist {
         songs = null;
         playlistName = null;
         playlistID = -1;
+    }
+
+    /******* All the functions below implement the Parcelable interface. *******/
+
+    // Write object values to parcel for storage.
+    public void writeToParcel(Parcel dest, int flags){
+        dest.writeString(playlistName);
+        dest.writeInt(playlistID);
+        dest.writeTypedList(songs);
+    }
+
+    // Constructor used for parcel.
+    public Playlist(Parcel parcel){
+        playlistName = parcel.readString();
+        playlistID = parcel.readInt();
+        parcel.readTypedList(songs, Song.CREATOR);
+    }
+
+    // Creator - used when un-parceling our parcel (creating the object).
+    public static final Parcelable.Creator<Playlist> CREATOR = new Parcelable.Creator<Playlist>(){
+        @Override
+        public Playlist createFromParcel(Parcel parcel) {
+            return new Playlist(parcel);
+        }
+
+        @Override
+        public Playlist[] newArray(int size) {
+            return new Playlist[size];
+        }
+    };
+
+    // Return hashcode of object.
+    public int describeContents() {
+        return hashCode();
     }
 }
