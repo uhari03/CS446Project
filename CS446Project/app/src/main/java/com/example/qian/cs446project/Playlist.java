a1d8b0c28e8dbd99ae394cbe9d1fc7652d80ff44
Hari Udhayakumar <u.hari03@gmail.com>
2018-02-24 21:20:48 -0500

    Fix null linked list reference

--- CS446Project/app/src/main/java/com/example/qian/cs446project/Playlist.java
+++ CS446Project/app/src/main/java/com/example/qian/cs446project/Playlist.java
@@ -9,15 +9,14 @@ import java.util.LinkedList;
  * Class com.example.qian.cs446project.Playlist encapsulates multiple com.example.qian.cs446project.Song objects.
  */
 
-public class Playlist {
-    public LinkedList<Song> songs;
+public class Playlist implements Parcelable {
+    public LinkedList<Song> songs = new LinkedList<Song>();
 
     private String playlistName;
     private int playlistID;
 
     // Constructor simply initializes its members.
     public Playlist(String playlistName, int playlistID) {
-        songs = new LinkedList<Song>();
         this.playlistName = playlistName;
         this.playlistID = playlistID;
     }
